e3015bcb8c1a4b9899d5a07eec7eef5a1e67771f
Néstor Coppi <nestorcoppi@gmail.com>
2015-08-27 21:45:07 -0300

    Logger

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ function SessionController () {
 }
 
 SessionController.prototype.subscribe = function (socket) {
+  console.log('se llama a controller subscribe')
   this.redis.on('message', function (channel, msg) {
+    console.log('en redis msg')
     socket.emit('new-console-intervention', { link: msg });
   });
 
+  console.log('en redis subscribe')
   this.redis.subscribe('socketio-new-intervention');
 }
 
@@ -24,15 +27,19 @@ SessionController.prototype.unsubscribe = function () {
 
 
 io.on('connection', function (socket) {
-
+  console.log('abriendo conexion')
   sessionController = new SessionController
   sessionController.subscribe(socket);
 
+  console.log('subscripto')
+
   socket.on('new intervention', function (data) {
+    console.log('new intervention, emitting')
     socket.broadcast.emit('update interventions', { message: data['message'] })
   });
 
   socket.on('disconnect', function() {
+    console.log('desconecting')
     sessionController.unsubscribe()
   });
 });
