fa69398cce1e364fc37b444d67c94eb324f495c3
Néstor Coppi <nestorcoppi@gmail.com>
2019-04-29 21:06:16 -0300

    Dockerfile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.listen(8085);
 
 function SessionController () {
   this.redis = require('redis').createClient(
-    6379, 
+    6379,
     process.env['REDIS_HOST'] || process.env['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'
   )
 }
